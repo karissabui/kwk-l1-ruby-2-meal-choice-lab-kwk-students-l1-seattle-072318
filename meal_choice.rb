228cbd71cc3911452848279c3f6ead8fa5e16bb5
Karissa Bui <karissab002@gmail.com>
2018-07-24 18:06:45 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,17 +1,17 @@
 
-def breakfast(cereal="Frosted Flakes")
+def breakfast(cereal)
   puts "Any time, is the right time for #{cereal}!"
 end
 
-def lunch(sandwich= "Grilled Cheese")
+def lunch(sandwich)
  puts "Everybody loves a #{sandwich}!"
 end
 
-def dinner(fish= "Salmon")
+def dinner(fish)
   puts "Evening is the best time for #{fish}"
 end
 
-breakfast("breakfast")
-lunch("lunch")
-dinner("dinner")
+breakfast("Frosted Flakes")
+lunch("Grilled Cheese")
+dinner("Salmon")
 
